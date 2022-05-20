09550275b849addaa821e55c96eaf098b3d4c125
Emanuele <emanuele.dajello@gmail.com>
2022-05-21 00:49:27 +0200

    new

--- Barbie.py
+++ Barbie.py
@@ -1,31 +1,37 @@
+from pyLittle_json import Json
 
 class Barbie:
+
+    '''
+    class of storage "simple Json_object{key => value}
+
+    operation set(key,value)
+
+    operation get(key)->value
+
+    '''
     
     log = []
 
     def __init__(self):
-        self.key = []
-        self.value = []
+        self.me = {}
+        self.list_key = []
 
     def set(self, **kwargs):
         '''
         perform a set of a key value on the db
         '''
-
-
+        if len(kwargs.items()) > 1:
+            return "error"
         for key, value in kwargs.items():
-            self.key.append(key)
-            self.value.append(value)
-        
+            self.me[key] = value
+    
     
     def get(self, key):
         '''
         perform a search by a key
         '''
-        try:
-            return self.value[self.key.index(key)]
-        except:
-            return "error no value for this key"
+        return self.me.get(key)
 
     def store(self):
         pass
